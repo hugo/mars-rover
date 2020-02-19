1b6fd968b7c28f7da9b5a9feab3402493995e448
Hugo Jobling <hello@thisishugo.com>
2020-02-19 12:19:21 +0000

    Do the needful

--- index.js
+++ index.js
@@ -1,1 +1,98 @@
-module.exports = {}
+class Rover {
+  constructor([x, y, heading], instructions) {
+    this.x = x
+    this.y = y
+    this.heading = heading
+    this.instructions = instructions
+  }
+
+  get position() {
+    return `${this.x}${this.y}${this.heading}`
+  }
+
+  explore() {
+    this.instructions.split('').forEach((instruction) => {
+      switch (instruction) {
+        case 'L':
+          this.turnLeft()
+          break
+        case 'R':
+          this.turnRight()
+          break
+        case 'M':
+          this.move()
+          break
+        default:
+          throw new Error(`Unexpected instruction ${instruction}`)
+      }
+    })
+
+    return this
+  }
+
+  turnLeft() {
+    this.heading = {N: 'W', E: 'N', S: 'E', W: 'S'}[this.heading]
+
+    return this
+  }
+
+  turnRight() {
+    this.heading = {N: 'E', E: 'S', S: 'W', W: 'N'}[this.heading]
+    return this
+  }
+
+  move() {
+    switch (this.heading) {
+      case 'N':
+        this.y++
+        break
+      case 'E':
+        this.x++
+        break
+      case 'S':
+        this.y--
+        break
+      case 'W':
+        this.x--
+        break
+      default:
+        throw new Error(`Unexpected heading ${this.heading}`)
+    }
+
+    return this
+  }
+}
+
+class Squad {
+  constructor(input) {
+    const [coordinates, roverInfos] = this.parse(input)
+    this.coordinates = coordinates
+
+    this.rovers = roverInfos.map((info) => new Rover(...info))
+  }
+
+  parse(input) {
+    const [coordinates, ...rest] = input.split('\n')
+
+    let roverInfos = []
+    while (rest.length) {
+      roverInfos.push([rest.shift(), rest.shift()])
+    }
+
+    return [coordinates, roverInfos]
+  }
+
+  explore() {
+    this.rovers.forEach((rover) => {
+      rover.explore()
+    })
+
+    return this
+  }
+
+  get roverPositions() {
+    return this.rovers.map((rover) => rover.position).join('\n')
+  }
+}
+
+module.exports = {Squad, Rover}
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {} = require('.')
+const {Squad, Rover} = require('.')
 
 const input = `
 55
@@ -13,8 +13,36 @@ const expected = `
 51E
 `.trim()
 
+describe('Rover', () => {
+  test('#explore', () => {
+    expect(new Rover('12N', 'LMLMLMLMM').explore().position).toBe('13N')
+    expect(new Rover('33E', 'MMRMMRMRRM').explore().position).toBe('51E')
+  })
+
+  test('#turnLeft', () => {
+    expect(new Rover('00N').turnLeft().position).toBe('00W')
+    expect(new Rover('00E').turnLeft().position).toBe('00N')
+    expect(new Rover('00S').turnLeft().position).toBe('00E')
+    expect(new Rover('00W').turnLeft().position).toBe('00S')
+  })
+
+  test('#turnRight', () => {
+    expect(new Rover('00N').turnRight().position).toBe('00E')
+    expect(new Rover('00E').turnRight().position).toBe('00S')
+    expect(new Rover('00S').turnRight().position).toBe('00W')
+    expect(new Rover('00W').turnRight().position).toBe('00N')
+  })
+
+  test('#move', () => {
+    expect(new Rover('11N').move().position).toBe('12N')
+    expect(new Rover('11E').move().position).toBe('21E')
+    expect(new Rover('11S').move().position).toBe('10S')
+    expect(new Rover('11W').move().position).toBe('01W')
+  })
+})
+
 test('explore', () => {
-  const actual = input
+  const actual = new Squad(input).explore().roverPositions
 
   expect(actual).toBe(expected)
 })
